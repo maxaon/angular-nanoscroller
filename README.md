527f807f01cff0998e17f170d3443c66caa25790
maxaon <maxaon@xakep.ru>
2014-01-11 19:37:32 +0200

    Documentation update

--- README.md
+++ README.md
@@ -1,4 +1,71 @@
 # Angular nanoScroller
 
-
 Wrapper for [nanoScrollerJS](http://jamesflorentino.github.io/nanoScrollerJS/)
+with AngularJS lifecycle integration.
+
+## Installation
+
+To use it include `scrollable.js` above your app.
+Also yiu mus include [nanoScrollerJS](http://jamesflorentino.github.io/nanoScrollerJS/) files.
+```html
+<link rel="stylesheet" href="nanoscroller.css"/>
+<script src="jquery.nanoscroller.js" type="text/javascript"></script>
+<script src="scrollable.js" type="text/javascript"></script>
+```
+
+Also you can use bower:
+`bower install angular-nanoscroller`
+
+Next, make sure to add **`sun.scrollable`** to your Angular app/module requirements:
+```javascript
+var module = angular.module('app', ['sun.scrollable']);
+```
+
+## Usage
+
+Scroll can be added as element:
+```html
+<div class="some-container">
+  <scrollable>
+    ... Some content ...
+  </scrollable>
+</div>
+```
+
+Or as attribute:
+```html
+<div class="some-container" scrollable>
+  ... Some content ...
+</div>
+```
+
+To use custom CSS classes, over root template tag, use element syntax.
+Parameters of the nanoScroller can be supplied as the value of the attribute `scrollable`,
+or as `scrollable` element attributes.
+```html
+<div class="some-container">
+  <scrollable class="greenScrollbar" always-visible="true" slider-max-height="200">
+    ... Some content ...
+  </scrollable>
+</div>
+OR
+<div class="some-container" scrollable="{alwaysVisible='true'}">
+  ... Some content ...
+</div>
+```
+
+Using attribute `static` will disable automatic scrollbar reconfiguration
+when height of content is changed.
+
+## Configuration
+To configure `scrollable` change constant `scrollableConfig`.
+
+Available parameters:
+* `template` - Template of the scroller. Uses `ng-transclude` for directive transclusion
+* `bottomMargin` -  Available number of pixels from the bottom,
+in which it is considered that scroller is in bottom. Default is '40'
+
+To set default parameters of the nanoScroller modify constant `nanoScrollerDefaults`.
+All parameters will be passed to nanoScroller during configuration stage.
+
+
--- scrollable.js
+++ scrollable.js
@@ -1,74 +1,108 @@
 ;
 (function (angular, $, undefined) {
   'use strict';
-  if ($.fn.nanoScroller === undefined)
+  if ($.fn['nanoScroller'] === undefined)
     throw new Error("nanoScrollerJS is not defined in jQuery");
   //jQuery must be used, cause angular method 'find' has different behavior
   if (angular.element !== $)
     throw new Error("Angular must use jQuery not jqLite");
 
-  var AS_ELEMENT = true, AS_ATTRIBUTE = false;
-  angular.module('sun.scrollable', ['ng'])
-    .constant("scrollableDefaults", {
-      template: '<div class="nano"><div class="content" ng-transclude></div></div>',
-      bottomMargin: 40
-    })
-    .constant("nanoScrollerDefaults", {
-    })
-    .directive("scrollable", createScrollableDirective(AS_ELEMENT))
-    .directive("scrollable", createScrollableDirective(AS_ATTRIBUTE));
+  const AS_ELEMENT = 1, AS_ATTRIBUTE = 0;
+  /**
+   * Wrapper of nanoScrollerJS
+   * @name sun.scrollable
+   */
+  var module = angular.module('sun.scrollable', ['ng']);
 
+  /**
+   * Configuration for the directive
+   * @name scrollableConfig
+   * @param scrollableConfig.template Template of the scroller
+   * @param scrollableConfig.bottomMargin Available number of pixels from the bottom,
+   *        in which it is considered that scroller is in bottom
+   *
+   */
+  module.constant("scrollableConfig", {
+    template: '<div class="nano"><div class="content" ng-transclude></div></div>',
+    bottomMargin: 40
+  });
+
+  /**
+   * Default configuration of the nanoScroller
+   * @name nanoScrollerDefaults
+   */
+  module.constant("nanoScrollerDefaults", {
+  });
+  module.directive("scrollable", createScrollableDirective(AS_ELEMENT));
+  module.directive("scrollable", createScrollableDirective(AS_ATTRIBUTE));
+
+  /**
+   * Create directive 'scrollable' specified by variable `type`.
+   * If type equals to `AS_ELEMENT` directive will replace element, otherwise template will be appended.
+   * @param type Type of the directive: `AS_ELEMENT` or `AS_ATTRIBUTE`
+   * @returns {function} Directive creation function
+   */
   function createScrollableDirective(type) {
-    return ['$timeout', 'scrollableDefaults', 'nanoScrollerDefaults',
-      function ($timeout, scrollableDefaults, nanoScrollerDefaults) {
-        return {
-          transclude: true,
-          replace: type === AS_ELEMENT,
-          restrict: type === AS_ELEMENT ? 'E' : 'A',
-          priority: 1000,
-          template: scrollableDefaults.template,
-          link: function (scope, element, attr) {
-            var contentElement = element.find('.content')[0],
-            // Find element with nano class including current
-              nanoElement = element.hasClass('nano') ? element : element.find('.nano'),
-              options = type === AS_ELEMENT ? convertStringToValue(attr) : scope.$eval(attr['scrollable']),
-              parentElement = contentElement.parentElement;
-            options = angular.extend({}, nanoScrollerDefaults, options);
-            if ('static' in attr) {
-              // Call scroller after transclusion
-              $timeout(nanoElement.nanoScroller.bind(nanoElement, options));
-            }
-            else {
-              scope.$watch( // Call nanoScroller, when height of content is changed
-                function () {
-                  return contentElement.scrollHeight;
-                },
-                function (newHeight, oldHeight) {
-                  if (newHeight === oldHeight) {
-                    nanoElement.nanoScroller(options)
-                  }
-                  else if (newHeight !== oldHeight && contentElement.scrollTop &&
-                    (oldHeight - contentElement.scrollTop - parentElement.clientHeight) < scrollableDefaults.bottomMargin) {
-                    nanoElement.nanoScroller({scroll: 'bottom'});
-                  }
-                  else {
-                    nanoElement.nanoScroller();
-                  }
-                });
-            }
-            scope.$on("$destroy", function () {
-              nanoElement.nanoScroller({ destroy: true });
-            })
+    var directive = function ($timeout, scrollableConfig, nanoScrollerDefaults) {
+      return {
+        transclude: true,
+        replace: type === AS_ELEMENT,
+        restrict: type === AS_ELEMENT ? 'E' : 'A',
+        priority: 1000,
+        template: scrollableConfig.template,
+        link: function (scope, element, attr) {
+          var contentElement = element.find('.content')[0],
+          // Find element with nano class including current
+            $nanoElement = element.hasClass('nano') ? element : element.find('.nano'),
+            parentElement = contentElement.parentElement,
+            options = angular.extend({}, nanoScrollerDefaults, convertStringToValue(attr), scope.$eval(attr['scrollable']));
+          if ('static' in attr) {
+            // Call scroller after transclusion
+            $timeout($nanoElement.nanoScroller.bind($nanoElement, options));
           }
-        };
-      }]
+          else {
+            scope.$watch( // Call nanoScroller, when height of content is changed
+              function () {
+                return contentElement.scrollHeight;
+              },
+              function (newHeight, oldHeight) {
+                // If this is first run, create nanoScroller
+                if (newHeight === oldHeight) {
+                  $nanoElement.nanoScroller(options)
+                }
+                //If scroller was on the bottom, scroll to bottom
+                else if (newHeight !== oldHeight && contentElement.scrollTop &&
+                  (oldHeight - contentElement.scrollTop - parentElement.clientHeight) < scrollableConfig.bottomMargin) {
+                  $nanoElement.nanoScroller({scroll: 'bottom'});
+                }
+                // Otherwise just update the pane
+                else {
+                  $nanoElement.nanoScroller();
+                }
+              });
+          }
+          scope.$on("$destroy", function () {
+            $nanoElement.nanoScroller({ destroy: true });
+            $nanoElement = contentElement = parentElement = null;
+          })
+        }
+      };
+    };
+    directive.$inject = ['$timeout', 'scrollableConfig', 'nanoScrollerDefaults'];
+    return directive;
   }
 
+  /**
+   * Convert element attributes from stings to objects.
+   * Also filter attribute (starting from '$' and class)
+   * @param attr Attribute array
+   * @returns {{}} Filtered object of converted attributes
+   */
   function convertStringToValue(attr) {
-    var res = {};
+    var result = {};
     angular.forEach(attr, function (value, key) {
       if (key.indexOf("$") === 0)
-        return
+        return;
       switch (key) {
         case "true":
           value = true;
@@ -82,8 +116,9 @@
         case "class":
           return;
       }
-      res[key] = value;
+      result[key] = value;
     });
-    return res
+    return result
   }
+
 }(angular, jQuery));
